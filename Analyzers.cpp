bee99d54cdc6318c5a18166ec7738a227df8da4d
chuanca26 <chuancao@unsa.edu.pe>
2024-07-11 17:10:17 -0500

    analizador sintactico corregido

--- Analyzers.cpp
+++ Analyzers.cpp
@@ -9,25 +9,22 @@ bool Analyzer::getStatus(string query)
   {
     Parser parser(tokens);
     Node* ast = parser.parse();
-    //parser.print_ast(ast);
+    parser.print_ast(ast);
 
     if (!validate_ast(ast))
+    {
       return 0;
-      /*
-        std::cout << "La consulta es semanticamente valida." << std::endl;
-    } else {
-        std::cout << "La consulta tiene errores semanticos." << std::endl;
+      std::cout << "La consulta tiene errores semanticos." << std::endl;
     }
-    */
   }
   catch (const SyntaxError& e)
   {
-    //status = e.what();
+    cout <<  e.what();
     return 0;
   }
   catch (const std::exception& e) 
   {
-    //status = e.what();
+    cout <<  e.what();
     return 0;
   }
   return 1;
