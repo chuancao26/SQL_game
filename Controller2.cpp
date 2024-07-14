d8792e0372d38e80577e8ea98bd3aa4b7daa763d
j4zzzz <jperezvi@unsa.edu.pe>
2024-07-14 10:30:07 -0500

    Actualizando el nivel de memoria, arreglando el enlace entre las palabras y las imágenes

--- Controller2.cpp
+++ Controller2.cpp
@@ -10,7 +10,7 @@
 
 class Cuadrado {
 public:
-    Cuadrado(int id, const sf::Texture& texOriginal, const sf::Texture& texPalabra, sf::Vector2f originalPos);
+    Cuadrado(int id, const sf::Texture& texOriginal, const sf::Texture& texPalabra, sf::Vector2f originalPos, const std::string& palabra);
     void draw(sf::RenderWindow& window);
     void moveToTarget(sf::Vector2f targetPos);
     void returnToOriginal();
@@ -21,6 +21,7 @@ public:
     void animateMovement(sf::Vector2f startPos, sf::Vector2f endPos);
     void incrementClickCount();
     int getClickCount() const;
+    std::string getPalabra() const;
 
 private:
     int id;
@@ -35,16 +36,18 @@ private:
     sf::Clock animationClock;
     float animationDuration; // Duration of the animation in seconds
     int clickCount; // Click counter
+    std::string palabra; // Associated word
 };
 
-Cuadrado::Cuadrado(int id, const sf::Texture& texOriginal, const sf::Texture& texPalabra, sf::Vector2f originalPos)
-    : id(id), originalPos(originalPos), currentPos(originalPos), moved(false), animating(false), animationDuration(0.5f), clickCount(0) {
+Cuadrado::Cuadrado(int id, const sf::Texture& texOriginal, const sf::Texture& texPalabra, sf::Vector2f originalPos, const std::string& palabra)
+    : id(id), originalPos(originalPos), currentPos(originalPos), moved(false), animating(false), animationDuration(0.5f), clickCount(0), palabra(palabra) {
     spriteOriginal.setTexture(texOriginal);
     spriteOriginal.setPosition(originalPos);
     spriteOriginal.setScale(125.0f / texOriginal.getSize().x, 100.0f / texOriginal.getSize().y); // Reduce the size to half
 
     spritePalabra.setTexture(texPalabra);
-    spritePalabra.setScale(50.0f / texPalabra.getSize().x, 62.5f / texPalabra.getSize().y); // Reduce the size to half
+    spritePalabra.setPosition(originalPos); // Set initial position
+    spritePalabra.setScale(75.0f / texPalabra.getSize().x, 75.0f / texPalabra.getSize().y); // Reduce the size to half
 }
 
 void Cuadrado::draw(sf::RenderWindow& window) {
@@ -118,6 +121,10 @@ int Cuadrado::getClickCount() const {
     return clickCount;
 }
 
+std::string Cuadrado::getPalabra() const {
+    return palabra;
+}
+
 // Clase Controller2
 
 class Controller2 {
@@ -169,23 +176,36 @@ Controller2::Controller2(sf::RenderWindow& window, int n)
     std::iota(positions.begin(), positions.end(), 0);
     std::shuffle(positions.begin(), positions.end(), std::mt19937{ std::random_device{}() });
 
+    std::vector<int> imgIndices(n * n);
+    std::iota(imgIndices.begin(), imgIndices.end(), 0);
+    std::shuffle(imgIndices.begin(), imgIndices.end(), std::mt19937{ std::random_device{}() });
+
+    std::string palabras[19] = {"SELECT", "*", "FROM", "producto", "nombre", 
+    "descripcion", "precio", "orden", "id", "user_id", "amount", "date",
+    "persona", "apellidos", "dni", "users", "name", "age"};
+
+    // Load imgPalabraTextures in the exact order of palabras
+    for (int i = 0; i < n * n; ++i) {
+        imgPalabraTextures.push_back(std::make_unique<sf::Texture>());
+        imgPalabraTextures[i]->loadFromFile("img/imgPalabra" + std::to_string(i + 1) + ".png");
+    }
+
     for (int i = 0; i < n * n; ++i) {
         imgOrdenTextures.push_back(std::make_unique<sf::Texture>());
-        imgPalabraTextures.push_back(std::make_unique<sf::Texture>());
+        imgOrdenTextures[i]->loadFromFile("img/imgOrden" + std::to_string(imgIndices[i] + 1) + ".png");
+    }
 
-        imgOrdenTextures[i]->loadFromFile("img/imgOrden" + std::to_string(i + 1) + ".png");
-        imgPalabraTextures[i]->loadFromFile("img/imgPalabra" + std::to_string(i + 1) + ".png");
-
+    for (int i = 0; i < n * n; ++i) {
         float x = (positions[i] % n) * (window.getSize().x * 2 / 4 / n) + 40;
         float y = (positions[i] / n) * (window.getSize().y * 4 / 6 / n) + 40;
         sf::Vector2f originalPos(x, y);
 
-        cuadrados.emplace_back(i, *imgOrdenTextures[i], *imgPalabraTextures[i], originalPos);
+        cuadrados.emplace_back(i, *imgOrdenTextures[i], *imgPalabraTextures[i], originalPos, palabras[i]);
     }
 
-    tablaTexture->loadFromFile("img/Tabla.png");
+    tablaTexture->loadFromFile("img/tabla.png");
     tablaSprite->setTexture(*tablaTexture);
-    tablaSprite->setPosition(window.getSize().x * 2 / 3, 100);
+    tablaSprite->setPosition(window.getSize().x * 2 / 3, 0);
 
     botonTexture1->loadFromFile("img/boton1.png"); // Load button 1 texture
     botonTexture2->loadFromFile("img/boton2.png"); // Load button 2 texture
@@ -275,12 +295,8 @@ void Controller2::handleRedSquareClick() {
 }
 
 void Controller2::resultadoPalabra(const std::vector<int>& indices) {
-    std::string palabras[19] = {"SELECT", "*", "FROM", "producto","nombre", 
-    "descripcion", "precio", "orden","id", "user_id", "amount", "date",
-    "persona", "nombre", "apellidos", "dni", "users","name", "age"};
-
     for (int indice : indices) {
-        std::cout << palabras[indice - 1] << " ";
+        std::cout << cuadrados[indice - 1].getPalabra() << " ";
     }
     std::cout << std::endl;
 }
